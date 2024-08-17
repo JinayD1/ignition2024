a4fb38b8aa6ac89410310962f77c15260e870207
ccjus <justinlhchu@gmail.com>
2024-08-17 18:25:05 -0400

    Update page.tsx
    added some new buttons

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -2,26 +2,38 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 import { login } from '@/actions'
+import "./login.css"
 
-const page = () => {
+const Page = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [message, setMessage] = useState('')
+
   return (
-    <>
-        <h4>Log In</h4>
-        <form onSubmit={(e) => {
-          e.preventDefault()
-          login(email, password)
-          }}>
-            <input type="email" placeholder='email' value={email} onChange={e => setEmail(e.target.value)}/>
-            <input type="password" placeholder='password' value={password} onChange={e => setPassword(e.target.value)}/>
-            <input type="submit" name='Submit'/>
-            <h6>{message}</h6>
-        </form>
-        <p>Don't have an account?&nbsp;</p><Link href='/sign_up'>Sign up!</Link>
-    </>
+    <div className="container">
+      <h4>Login</h4>
+      <p>Have an account?</p>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        login(email, password)
+      }}>
+        <input type="email" placeholder='Username' value={email} onChange={e => setEmail(e.target.value)}/>
+        <input type="password" placeholder='Password' value={password} onChange={e => setPassword(e.target.value)}/>
+        <input type="submit" value="SIGN IN"/>
+        <div className="remember-forgot">
+          <label><input type="checkbox"/> Remember Me</label>
+          <a href="#">Forgot Password</a>
+        </div>
+      </form>
+      {message && <h6>{message}</h6>}
+      <p>— Or Sign In With —</p>
+      <div className="social-login">
+        <button>Facebook</button>
+        <button>Google</button>
+      </div>
+      <p>Don't have an account? <Link href='/sign_up' className="signup-link">Sign up!</Link></p>
+    </div>
   )
 }
 
-export default page
+export default Page
