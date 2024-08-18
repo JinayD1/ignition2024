d9ad5de344cbe628ca5ae73ba7094e1aca60af4a
ccjus <justinlhchu@gmail.com>
2024-08-17 22:56:37 -0400

    Update NoteDoc.tsx
    changed the format + imported css file

--- frontend/app/components/NoteDoc.tsx
+++ frontend/app/components/NoteDoc.tsx
@@ -1,11 +1,16 @@
 import Link from 'next/link'
 import React from 'react'
+import './NoteDoc.css' 
 
 const NoteDoc = (props: {name: string, id: number}) => {
   return (
-        <div style={{borderStyle: 'solid', borderColor: 'black', borderRadius: '10px', borderWidth: '4px', width: '100px', height: '200px'}}>
-            <p>{props.name}</p>
+    <div className="note-doc-container">
+      <Link href={`/edit_note_doc/${props.id}`} className="note-doc-link">
+        <div className="note-doc">
+          <p className="note-doc-name">{props.name}</p>
         </div>
+      </Link>
+    </div>
   )
 }
 
